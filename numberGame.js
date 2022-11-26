ae4303ec4e4f61a0231509bd6cce216b543d90ea
mridubhatnagar <mridubhatnagar4@gmail.com>
2022-11-26 12:33:59 +0530

    added randomNumber generation logic

--- numberGame.js
+++ numberGame.js
@@ -1,7 +1,35 @@
 class Board {
     constructor(boardSize, containerDiv) {
         this.boardSize = boardSize,
-        this.containerDiv = containerDiv
+        this.containerDiv = containerDiv,
+        this.cellArray=new Array(this.boardSize)
+        for( let x=0;x<this.boardSize;x++) {
+            this.cellArray[x]=new Array(this.boardSize);    
+        }       
+    }
+
+    generateListOfNumbers() {
+        const arrayOfNumbers = []
+        for(let i=0; i<this.boardSize*this.boardSize; i++) {
+            arrayOfNumbers.push(i);
+        }
+        console.log(arrayOfNumbers);
+        return arrayOfNumbers 
+    }
+
+    getRandomNumber(numberList) {
+        console.log(numberList);
+        if (numberList.length >= 1) {
+            let result = Math.floor((Math.random() * (numberList.length-1)));
+            console.log(`Random Number Value: ${numberList[result]}`)
+            return numberList[result]
+        }
+    }
+
+    removeNumberFromArray(number, numberList) {
+        let y = numberList.indexOf(number);
+        let x = numberList.splice(y, 1);
+        console.log(`Number popped out from list: ${x}`);
     }
 
     render() {
@@ -10,14 +38,18 @@ class Board {
         const divTableBody = document.createElement("div");
         divTableBody.setAttribute("class", "divBody");
         divTable.appendChild(divTableBody);
-        for (let i = 0; i < this.boardSize; i++) {
+        const numberList = this.generateListOfNumbers();
+        for (let row = 0; row < this.boardSize; row++) {
             const divTableRow = document.createElement("div");
             divTableRow.setAttribute("class", "divTableRow");
             divTableBody.appendChild(divTableRow);
-            for (let j=0; j < this.boardSize; j++) {
+            for (let column=0; column < this.boardSize; column++) {
+                const number = this.getRandomNumber(numberList);
+                this.removeNumberFromArray(number, numberList);
+                // this.cellArray[row][column]=new cell(this.board,row,column,number);
                 const divTableCell = document.createElement("div");
-                divTableCell.id = `div_${i}${j}`;
-                divTableCell.innerHTML = `${i}${j}`;
+                divTableCell.id = `div_${row}${column}`;
+                divTableCell.innerHTML = `${row}${column}`;
                 divTableCell.setAttribute("class", `divTableCell`);
                 divTableRow.appendChild(divTableCell);
             }
